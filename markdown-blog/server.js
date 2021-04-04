0e84235dee289f03108cffbd91411754497a2fa5
kyleschmitz1 <kylehudson125@msn.com>
2021-04-04 10:52:05 -0400

    initial setup of package.json and server.js

---
+++ markdown-blog/server.js
@@ -0,0 +1,11 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send('Hello, World')
+})
+app.listen(5000)
