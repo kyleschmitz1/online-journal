b3e145436a8549c875c32a54d633e7eb2ce719f1
kyleschmitz1 <kylehudson125@msn.com>
2021-04-04 15:09:59 -0400

    30 minutes following along with wds markdown blog

--- markdown-blog/server.js
+++ markdown-blog/server.js
@@ -1,11 +1,30 @@
 const { response } = require('express')
 const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', { useUnifiedTopology: true , useNewUrlParser: true})
 
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({extended: false}))
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello, World')
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test Description'
+    },{
+        title: 'Test Article 2',
+        createdAt: new Date(),
+        description: 'Test Description 2'
+    }]
+    res.render('articles/index', {articles: articles})
 })
 app.listen(5000)
+
+app.use('/articles', articleRouter)
